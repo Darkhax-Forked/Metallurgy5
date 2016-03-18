171eacaba328962ac83d1b2f9e490519123bb151
Glassmaker <JeanGlassmaker@mail.com>
2016-03-18 19:46:46 +0300

    Adds framework

---
+++ core/src/main/java/com/teammetallurgy/metallurgy/core/MetallurgyCore.java
@@ -0,0 +1,29 @@
+package com.teammetallurgy.metallurgy.core;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = MetallurgyCore.MODID, version = MetallurgyCore.VERSION)
+public class MetallurgyCore {
+
+    public static final String MODID = "metallurgy_core";
+    public static final String VERSION = "5.0.0";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ precious/src/main/java/com/teammetallurgy/metallurgy/precious/MetallurgyPrecious.java
@@ -0,0 +1,29 @@
+package com.teammetallurgy.metallurgy.precious;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = MetallurgyPrecious.MODID, version = MetallurgyPrecious.VERSION)
+public class MetallurgyPrecious {
+
+    public static final String MODID = "metallurgy_precious";
+    public static final String VERSION = "5.0.0";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
