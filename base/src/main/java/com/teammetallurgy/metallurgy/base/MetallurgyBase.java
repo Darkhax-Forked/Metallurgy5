b8e134eed1fc03bffac4d0a0013e1dae932e8862
Glassmaker <JeanGlassmaker@mail.com>
2016-03-18 20:35:41 +0300

    Fixes Metallurgy base modid

--- base/src/main/java/com/teammetallurgy/metallurgy/base/MetallurgyBase.java
+++ base/src/main/java/com/teammetallurgy/metallurgy/base/MetallurgyBase.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = MetallurgyBase.MODID, version = MetallurgyBase.VERSION)
 public class MetallurgyBase {
 	
-    public static final String MODID = "metallurgy_core";
+    public static final String MODID = "metallurgy_base";
     public static final String VERSION = "5.0.0";
     
     @EventHandler
