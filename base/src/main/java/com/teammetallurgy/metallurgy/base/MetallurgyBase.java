1f8f4a9b28c2498168ce4664e09ee310f0714afb
Glassmaker <JeanGlassmaker@mail.com>
2016-03-18 21:57:21 +0300

    Merging core with base

--- base/src/main/java/com/teammetallurgy/metallurgy/base/MetallurgyBase.java
+++ base/src/main/java/com/teammetallurgy/metallurgy/base/MetallurgyBase.java
@@ -6,12 +6,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MetallurgyBase.MODID, version = MetallurgyBase.VERSION, dependencies = MetallurgyBase.DEPS)
+@Mod(modid = MetallurgyBase.MODID, version = MetallurgyBase.VERSION)
 public class MetallurgyBase {
 	
     public static final String MODID = "metallurgy_base";
     public static final String VERSION = "5.0.0";
-    public static final String DEPS = "required-after:metallurgy_core";
+    
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
--- precious/src/main/java/com/teammetallurgy/metallurgy/precious/MetallurgyPrecious.java
+++ precious/src/main/java/com/teammetallurgy/metallurgy/precious/MetallurgyPrecious.java
@@ -6,11 +6,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MetallurgyPrecious.MODID, version = MetallurgyPrecious.VERSION)
+@Mod(modid = MetallurgyPrecious.MODID, version = MetallurgyPrecious.VERSION, dependencies = MetallurgyPrecious.DEPS)
 public class MetallurgyPrecious {
 
     public static final String MODID = "metallurgy_precious";
     public static final String VERSION = "5.0.0";
+    public static final String DEPS = "required-after:metallurgy_base";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
